169f1bf472ec76a359badba5ccc1d2e340b29202
Ethan McCutchen <ethan@grasscommons.org>
2012-12-01 17:21:49 -0700

    fixed packs and got rid of old migrate script

--- wagn-fortzed/files-ws/fortzed_pack.rb
+++ wagn-fortzed/files-ws/fortzed_pack.rb
@@ -5,7 +5,7 @@ module Wagn::Set::FortZed
   format :html
   
   define_view :core, :name=>'pledge_count' do |args|
-    Session.as :wagn_bot do
+    Account.as_bot do
       # would be great to have a better solution for this!
       _final_search_type_type_core args
     end
