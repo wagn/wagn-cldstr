33ccadf00b119879abefaf383bd845e2844cdb68
Ethan McCutchen <ethan@grasscommons.org>
2013-03-29 15:53:59 -0600

    auto-pulled latest wagn code

--- wagn-cp/files-ws/cp_pack.rb
+++ wagn-cp/files-ws/cp_pack.rb
@@ -198,9 +198,9 @@ module Wagn
   class Renderer::Json 
     # bit of a hack to make navbox results restrictable
     def goto_wql(term)
-     xtra = search_params
-     xtra.delete :default_limit
-     xtra.merge( { :complete=>term, :limit=>8, :sort=>'name', :return=>'name' } )
+      xtra = search_params
+      xtra.delete :default_limit
+      xtra.merge( { :complete=>term, :limit=>8, :sort=>'name', :return=>'name' } )
     end
   end
 
