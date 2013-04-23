825b68ef7e5074d5a77cdc8dd5a5fb33c34af1cc
Ethan McCutchen <ethan@grasscommons.org>
2013-04-23 11:44:15 -0600

    auto-pulled latest wagn code

--- wagn-cp/files-ws/cp_pack.rb
+++ wagn-cp/files-ws/cp_pack.rb
@@ -119,7 +119,6 @@ module Wagn
   
     define_view :raw, :name=>:cp_navbox do |args|
       %{ <form action="#{Card.path_setting '/:search'}" id="navbox-form" method="get">
-        #{hidden_field_tag :view, 'content' }
         #{hidden_field_tag :item, 'cp_result_item' }
         #{text_field_tag :_keyword, '', :class=>'navbox'
         }#{select_tag '_wql[type]', options_for_select([['All Content',nil], 'Foundations', 'Organizations', 'Topics', 'People'])
