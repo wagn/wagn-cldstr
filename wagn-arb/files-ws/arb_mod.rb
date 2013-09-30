634929c4b2a4f3c1073c3ef3db21d82d2d345b89
Ethan McCutchen <ethan@grasscommons.org>
2013-09-30 13:20:52 -0600

    auto-pulled latest wagn code

--- wagn-arb/files-ws/arb_mod.rb
+++ wagn-arb/files-ws/arb_mod.rb
@@ -1,6 +1,5 @@
  # -*- encoding : utf-8 -*-
 
-# /card/update/contact?card[codename]=arb_contact
 
 class Card
   def self.default_accounted_type_id
@@ -8,13 +7,32 @@ class Card
   end
   
   module Set
-    module Type::ArbContact
-      extend Set
-      event :set_contact_permissions, :on=>:create, :after=>:store do
-        Account.as_bot do
-          
-        end
+    module Type
+      module ArbIdea
+        extend Card::Set
         
+        event :require_arb_contact, :after=>:approve, :on=>:create do
+          unless c = cards['~plus~contacts'] and !c['content'].blank?
+            errors.add :contact, 'contact information required'
+          end
+        end
+      end
+      
+      
+      module ArbContact
+        extend Card::Set
+        event :set_contact_permissions, :after=>:activate_account do
+          Account.as_bot do
+            permitted = [ Card['Staff'], self ].map { |p| "[[#{p.name}]]" }.join("\n")
+            Card.create! :name=>"#{name}+*self+*update", :content=>permitted
+          
+            cr = creator
+            if creator != self && creator.codename != 'anonymous'
+              permitted << "\n[[#{creator.name}]]"
+            end
+            Card.create! :name=>"#{name}+*self+*read",   :content=>permitted
+          end
+        end
       end
     end
     
