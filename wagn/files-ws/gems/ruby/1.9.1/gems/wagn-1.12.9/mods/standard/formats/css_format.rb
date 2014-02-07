37d27892e60a93b3042d7cda6e673d45e0435991
Ethan McCutchen <ethan@grasscommons.org>
2014-02-07 12:24:04 -0700

    regenerate manifest

--- wagn/files-ws/gems/ruby/1.9.1/gems/wagn-1.12.9/mods/standard/formats/css_format.rb
+++
@@ -1,3 +0,0 @@
-# -*- encoding : utf-8 -*-
-class Card::CssFormat < Card::Format
-end
--- wagn/files-ws/gems/ruby/1.9.1/gems/wagn-1.12.9/mods/standard/formats/rss_format.rb
+++
@@ -1,8 +0,0 @@
-# -*- encoding : utf-8 -*-
-class Card::RssFormat < Card::HtmlFormat
-
-  def internal_url relative_path
-    wagn_url relative_path
-  end
-
-end
