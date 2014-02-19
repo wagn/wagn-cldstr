ef3d50821a8fa7972fd1bfcebe8bf32400e9dab7
Ethan McCutchen <ethan@grasscommons.org>
2014-02-19 13:48:36 -0700

    regenerate manifest

--- wagn/files-ws/gems/ruby/1.9.1/gems/wagn-1.12.11/mods/core/formats/text_format.rb
+++
@@ -1,8 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class Card
-  Format.register :text
-  class TextFormat < Format
-    @@aliases[:txt] = :text
-  end
-end
--- wagn/files-ws/gems/ruby/1.9.1/gems/wagn-1.12.11/spec/mods/standard/sets/right/style_spec.rb
+++
@@ -1,6 +0,0 @@
-# -*- encoding : utf-8 -*-
-require 'wagn/spec_helper'
-
-describe Card::Set::Right::Style do
-  # SPECSTUB (low priority)
-end
