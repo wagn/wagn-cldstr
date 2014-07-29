dbc9107f745287da6216138eec03fa0248bdef2a
Ethan McCutchen <ethan@grasscommons.org>
2014-07-29 14:53:46 -0600

    fixed cp_mod bug and ticked up wagn version

--- wagn-cp/files-ws/cp_mod/set/right/image.rb
+++ wagn-cp/files-ws/cp_mod/set/right/image.rb
@@ -3,7 +3,7 @@
 
 
 view :missing do |args|  # FIXME - implement using type_plus_right set when new functionality is in place
-  if left and left.type_id == Card::UserID
+  if card.left and card.left.type_id == Card::UserID
     wrap args do
       subformat( Card['missing person'] )._render_core
     end
