b8edfe04a68f876a821222d03c11a5ee0edbaff8
Ethan McCutchen <ethan@grasscommons.org>
2014-11-10 16:40:27 -0700

    red commit sweep

--- wagn/files-ws/gems/ruby/1.9.1/gems/wagn-1.12.13/mods/standard/sets/self/foot.rb
+++
@@ -1,9 +0,0 @@
-  # -*- encoding : utf-8 -*-
-
-format :html do
-  view :raw do |args|
-    '<!-- *foot is deprecated. please remove from layout -->'
-  end
-
-  view :core, :raw
-end
--- wagn/files-ws/gems/ruby/1.9.1/gems/wagn-1.12.13/spec/mods/standard/sets/all/file_spec.rb
+++
@@ -1,6 +0,0 @@
-# -*- encoding : utf-8 -*-
-require 'wagn/spec_helper'
-
-describe Card::Set::All::File do
-  # these are basically just fallback views
-end
