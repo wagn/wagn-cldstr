c52dd16f9728ba90aa48853ad71d2262bd596599
Ethan McCutchen <ethan@grasscommons.org>
2014-11-10 15:47:29 -0700

    updated wagn gem

---
+++ wagn/files-ws/gems/ruby/1.9.1/gems/wagn-1.12.13/db/migrate_cards/20130419215612_import_help_text.rb
@@ -0,0 +1,23 @@
+# -*- encoding : utf-8 -*-
+
+class ImportHelpText < ActiveRecord::Migration
+  include Wagn::MigrationHelper
+  def up
+    contentedly do
+      # generated JSON from template db with this url:  /*help+*right+by_name.json?view=core&pretty=true
+      json = File.read( File.join Wagn.gem_root, 'db/migrate_cards/data/1.11_help_text.json' )
+      data = JSON.parse json
+      Rails.logger.info "parsed!"
+      data.each do |atom|
+        c = atom['card']
+        Card.merge c['name'], { :type=>c['type'], :content=>atom['views'][0]['parts'] }, :pristine=>true
+      end      
+    end
+  end
+
+  def down
+    contentedly do
+      
+    end
+  end
+end
