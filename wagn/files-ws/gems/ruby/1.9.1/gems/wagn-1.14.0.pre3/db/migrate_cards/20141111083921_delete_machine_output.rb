6cb94c2f5b2bbbe8f5328f3f5f2fae444e98b358
Ethan McCutchen <ethan@grasscommons.org>
2014-11-14 22:36:02 -0700

    red commit sweep

--- wagn/files-ws/gems/ruby/1.9.1/gems/wagn-1.14.0.pre3/db/migrate_cards/20141111083921_delete_machine_output.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class DeleteMachineOutput < Wagn::Migration
-  def up
-    Card.search( :right => { :codename => 'machine_output' } ).each do |card|
-      card.delete!
-    end      
-  end
-end
