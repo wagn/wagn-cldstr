0625991199b249d5fde4b4f84e14197830133b82
Ethan McCutchen <ethan@grasscommons.org>
2014-11-14 22:34:57 -0700

    updated wagn gem

---
+++ wagn/files-ws/gems/ruby/1.9.1/gems/wagn-1.14.0/lib/wagn/all.rb
@@ -0,0 +1,16 @@
+require 'rails/all'
+
+require 'htmlentities'
+require 'recaptcha'
+require 'airbrake'
+require 'RMagick'
+require 'paperclip'
+require 'coderay'
+require 'haml'
+require 'kaminari'
+require 'diff/lcs'
+require 'diffy'
+#require 'scheduler_daemon'
+
+require 'wagn/application'
+
