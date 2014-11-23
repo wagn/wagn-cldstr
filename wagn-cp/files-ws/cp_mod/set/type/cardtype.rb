b8abc7f34bee7005ce6dd976d4fd92397314d3ab
Ethan McCutchen <ethan@grasscommons.org>
2014-11-22 23:20:59 -0700

    fixed cp mod following bug

--- wagn-cp/files-ws/cp_mod/set/type/cardtype.rb
+++ wagn-cp/files-ws/cp_mod/set/type/cardtype.rb
@@ -5,12 +5,12 @@ format :html do
       type_link = card.watched? ? "#{watching_type_cards} | " : ""
       plural = card.name.pluralize
       link_args = if card.watched?
-        ["Following", :off, "stop sending", { :hover_content=>"Stop Following all #{plural}" } ]
+        [card, "Following", :off, "stop sending", { :hover_content=>"Stop Following all #{plural}" } ]
       else
-        ["Follow", :on, "send"]
+        [card, "Follow", :on, "send"]
       end
-      link_args[0] += " all #{plural}"
-      link_args[2] += " emails about changes to #{plural}"
+      link_args[1] += " all #{plural}"
+      link_args[3] += " emails about changes to #{plural}"
       type_link + watch_link( *link_args )
     end
   end
