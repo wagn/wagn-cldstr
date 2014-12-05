6132e6d402660fe0429f51bae9d0ea62aae2e4a0
Ethan McCutchen <ethan@grasscommons.org>
2014-12-05 13:22:14 -0700

    rufus scheduler config

--- wagn-aikidoart/files-ws/email_remote_control/initializers/rufus_scheduler.rb
+++ wagn-aikidoart/files-ws/email_remote_control/initializers/rufus_scheduler.rb
@@ -1,16 +1,15 @@
-Wagn.config.gmail_user = 'wagnculture@gmail.com'
-Wagn.config.gmail_password = 'br8nd0nwc'
-Wagn.config.gmail_interval = '30s'
+WAGN_REMOTE_EMAIL_CONFIG = YAML.load_file("#{Wagn.root}/remote_email_config.yml")
 
-if Wagn.config.gmail_user
+
+if WAGN_REMOTE_EMAIL_CONFIG[:gmail_user]
     
   require 'rufus-scheduler'
 
   scheduler = Rufus::Scheduler.new
 
-  scheduler.every Wagn.config.gmail_interval do
+  scheduler.every WAGN_REMOTE_EMAIL_CONFIG[:gmail_interval] do
     Card::Set::Self::GmailRemote.check_mails
   end
   
 end
-#scheduler.join
+
