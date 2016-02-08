991d162f2a3f48e90b55d03bcddfb824d45ab4c1
Ethan McCutchen <ethan@grasscommons.org>
2016-02-08 11:21:22 -0700

    moved old stuff further away

--- old/wagn-cp/files-ws/cp_mod/set/self/search.rb
+++
@@ -1,7 +0,0 @@
-format :json do
-  def goto_wql(term)
-    xtra = search_params
-    xtra.delete :default_limit
-    xtra.merge( { :complete=>term, :limit=>8, :sort=>'name', :return=>'name' } )
-  end
-end
